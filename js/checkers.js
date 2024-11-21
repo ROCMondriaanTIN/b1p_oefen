d81f742dc3d41bb49667f87837e1d919370c7e17
Mark Kleijwegt <mark.kleijwegt@gmail.com>
2024-11-21 14:25:22 +0100

    set shadow

--- js/checkers.js
+++ js/checkers.js
@@ -32,7 +32,7 @@ for (let i = 0; i < blocks.length; i++) {
         block.innerHTML = '<div class="piece black border-white shadow-white"></div>';
     }
     if(boardArray[row][column] === 1){
-        block.innerHTML = '<div class="piece white border-black shadow-black"></div>';
+        block.innerHTML = '<div class="piece white border-black shadow-white"></div>';
     }
     column++;
     if(column === 8){
